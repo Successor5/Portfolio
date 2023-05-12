d67b54014835e5ce97ae27132ddb363ad7f34f02
Debbie <119403063+debby71711@users.noreply.github.com>
2023-05-12 09:35:46 +0100

    Add files via upload

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
+/*import React from 'react';
 import './App.css';
 import Portfolio from './pages/dashBoard/components/portfolio';
 import Project from './pages/dashBoard/components/Project';
 import Skills from './pages/dashBoard/components/skills';
 import About from './pages/dashBoard/components/about';
 import Me from './pages/dashBoard/components/me';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';*/
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
+       /* <Routes>
           <Route path="/" element={<Portfolio />} />
           <Route path="/project" element={<Project />} />
           <Route path="/Skills" element={<Skills/>} />
           <Route path="/About" element={<About/>} />
           <Route path="/Contact" element={<Me/>} />
-        </Routes>
+        </Routes>*/
       </Router>
     </div>
   );
